df2d847cf6e18850125166c015c0a178521e3f76
Csabbah <csabbah@ryerson.ca>
2022-04-16 21:54:31 -0400

    Developed a new ID generator function to execute when declaring new employee objects and included new comments

--- index.js
+++ index.js
@@ -4,39 +4,47 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
+function generateId() {
+  return Math.ceil(Math.random(0) * 10000);
+}
 function initializeProfiles() {
-  const intern1 = (this.intern = new Intern(
-    'Carlos',
-    Math.ceil(Math.random(0) * 10000),
-    'csabbah@ryerson.ca',
-    'Intern', // < If this is blank, use 'Employee' as the default role
-    'Ryerson'
-  ));
-  intern1.getSchool(); // This executes the intern method that simply console logs the school
-  intern1.getRole(); // This executes the intern method that simply console logs the school
-  // ------- ------- ------- ------- ------- ------- ------- ------- -------
-  const engineer1 = (this.engineer = new Engineer(
-    'Niv',
-    Math.ceil(Math.random(0) * 10000),
-    'Niv@hotmail.ca',
-    'Engineer', // < If this is blank, use 'Employee' as the default role
-    'Nimran'
-  ));
-  engineer1.getGithub();
-  engineer1.getRole();
-  // ------- ------- ------- ------- ------- ------- ------- ------- -------
-  const manager1 = (this.manager = new Manager(
-    'Grace',
-    Math.ceil(Math.random(0) * 10000),
-    'grace@gmail.ca',
-    'Manager', // < If this is blank, use 'Employee' as the default role
-    '124'
-  ));
-  manager1.getOfficeNumber();
-  manager1.getRole();
-  // ------- ------- ------- ------- ------- ------- ------- ------- -------
-  const employee = (this.employee = new Employee());
-  employee.getRole();
+  // const intern1 = (this.intern = new Intern(
+  //   'Carlos',
+  //   generateId(),
+  //   'csabbah@ryerson.ca',
+  //   'Intern', // < If this is blank, use 'Employee' as the default role
+  //   'Ryerson'
+  // ));
+  // console.log(intern1);
+  // intern1.getName()
+  // intern1.getSchool(); // This executes the intern method that simply console logs the school
+  // intern1.getRole(); // This executes the intern method that simply console logs the school
+  // // ------- ------- ------- ------- ------- ------- ------- ------- -------
+  // const engineer1 = (this.engineer = new Engineer(
+  //   'Niv',
+  //   generateId(),
+  //   'Niv@hotmail.ca',
+  //   'Engineer', // < If this is blank, use 'Employee' as the default role
+  //   'Nimran'
+  // ));
+  // console.log(engineer1);
+  // engineer1.getEmail();
+  // engineer1.getGithub();
+  // engineer1.getRole();
+  // // ------- ------- ------- ------- ------- ------- ------- ------- -------
+  // const manager1 = (this.manager = new Manager(
+  //   'Grace',
+  //   generateId(),
+  //   'grace@gmail.ca',
+  //   'Manager', // < If this is blank, use 'Employee' as the default role
+  //   '124'
+  // ));
+  // manager1.getId();
+  // manager1.getOfficeNumber();
+  // manager1.getRole();
+  // // ------- ------- ------- ------- ------- ------- ------- ------- -------
+  // const employee = (this.employee = new Employee());
+  // employee.getRole();
 }
 
 initializeProfiles();
