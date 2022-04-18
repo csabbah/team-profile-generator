13e77739dae1dfab24dda80a2ff29f78a5970a91
Csabbah <csabbah@ryerson.ca>
2022-04-17 20:16:12 -0400

    Added all necessary tests for intern class and added new function to assign 'Employee' role by default if role property is not assigned to any object

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,47 @@
 const Intern = require('../lib/Intern.js');
+
+function generateId() {
+  return Math.ceil(Math.random(0) * 10000);
+}
+
+const intern1 = new Intern(
+  'Carlos',
+  generateId(),
+  'csabbah@ryerson.ca',
+  'Intern',
+  'Ryerson'
+);
+
+const intern2 = new Intern(
+  'Carlos',
+  generateId(),
+  'csabbah@ryerson.ca',
+  '',
+  'Ryerson'
+);
+
+test('Creates intern object', () => {
+  expect(intern1.name).toEqual(expect.any(String));
+  expect(intern1.id).toEqual(expect.any(Number));
+  expect(intern1.email).toEqual(expect.stringContaining('@'));
+  expect(intern1.role).toEqual('Intern');
+  expect(intern1.school).toEqual(expect.any(String));
+});
+
+test('Unique ID generated', () => {
+  expect(generateId()).toEqual(expect.any(Number));
+});
+
+test('Intern method returns school', () => {
+  expect(intern1.getSchool()).toEqual(expect.any(String));
+});
+
+test('Intern method returns role', () => {
+  expect(intern1.getRole()).toEqual('Intern');
+});
+
+test('Returns employee if role property not filled', () => {
+  // Test to make sure both the getRole function and regular property return Employee as it should
+  expect(intern2.getRole()).toEqual('Employee');
+  expect(intern2.role).toEqual('Employee');
+});
--- index.js
+++ index.js
@@ -8,42 +8,42 @@ function generateId() {
   return Math.ceil(Math.random(0) * 10000);
 }
 function initializeProfiles() {
-  // const intern1 = (this.intern = new Intern(
+  // const intern1 = new Intern(
   //   'Carlos',
   //   generateId(),
   //   'csabbah@ryerson.ca',
   //   'Intern', // < If this is blank, use 'Employee' as the default role
   //   'Ryerson'
-  // ));
+  // );
   // console.log(intern1);
-  // intern1.getName()
+  // intern1.getName();
   // intern1.getSchool(); // This executes the intern method that simply console logs the school
   // intern1.getRole(); // This executes the intern method that simply console logs the school
   // // ------- ------- ------- ------- ------- ------- ------- ------- -------
-  // const engineer1 = (this.engineer = new Engineer(
+  // const engineer1 = new Engineer(
   //   'Niv',
   //   generateId(),
   //   'Niv@hotmail.ca',
   //   'Engineer', // < If this is blank, use 'Employee' as the default role
   //   'Nimran'
-  // ));
+  // );
   // console.log(engineer1);
   // engineer1.getEmail();
   // engineer1.getGithub();
   // engineer1.getRole();
   // // ------- ------- ------- ------- ------- ------- ------- ------- -------
-  // const manager1 = (this.manager = new Manager(
+  // const manager1 = new Manager(
   //   'Grace',
   //   generateId(),
   //   'grace@gmail.ca',
   //   'Manager', // < If this is blank, use 'Employee' as the default role
   //   '124'
-  // ));
+  // );
   // manager1.getId();
   // manager1.getOfficeNumber();
   // manager1.getRole();
   // // ------- ------- ------- ------- ------- ------- ------- ------- -------
-  // const employee = (this.employee = new Employee());
+  // const employee = new Employee();
   // employee.getRole();
 }
 
